1ef546f7d3143ea1f3e5e598c8da325712b78542
Og B. Maciel <ogmaciel@gnome.org>
2010-07-13 14:14:25 -0400

    Changed Location to Package in aurelio/admin.py as we don't have the Location class anymore.

--- bidu/aurelio/admin.py
+++ bidu/aurelio/admin.py
@@ -2,17 +2,17 @@
 # -*- encoding: utf-8 -*-
 
 from django.contrib import admin
-from aurelio.models import Sentence, Location, Word
+from aurelio.models import Sentence, Package, Word
 
 class SentenceAdmin(admin.ModelAdmin):
     pass
 
-class LocationAdmin(admin.ModelAdmin):
+class PackageAdmin(admin.ModelAdmin):
     pass
 
 class WordAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Sentence, SentenceAdmin)
-admin.site.register(Location, LocationAdmin)
+admin.site.register(Package, PackageAdmin)
 admin.site.register(Word, WordAdmin)
