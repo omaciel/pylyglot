612b7691a44b77068ccb8f7f1b387937e7c241a5
Og B. Maciel <ogmaciel@gnome.org>
2010-07-17 19:18:23 -0400

    Finally started implementing the layout Evandro gave me. Will iterate.

--- aurelio/forms.py
+++ aurelio/forms.py
@@ -5,7 +5,7 @@ from django import forms
 
 class SearchForm(forms.Form):
     query = forms.CharField(
-        label = u'Enter a word to search for',
+            label = u'Search for:',
         widget = forms.TextInput(attrs={'size': 32})
     )
 
