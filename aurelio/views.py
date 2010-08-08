ee4a782d120aeeb11396703a124ac158ca21e877
Og B. Maciel <ogmaciel@gnome.org>
2010-08-08 12:19:38 -0400

    Query on Translation class and filter by Language and Word.

--- aurelio/views.py
+++ aurelio/views.py
@@ -7,26 +7,32 @@ from aurelio.forms import *
 from aurelio.models import *
 
 def glossary_page(request):
-  form = SearchForm()
-  words = []
-  show_results = False
-  if 'query' in request.GET:
-     show_results = True
-     query = request.GET['query'].strip()
-     language = request.GET['languages'].strip()
-     if query:
-       form = SearchForm({'query' : query})
-       words = Word.objects.filter(
-         term__icontains=query
-       ).filter(sentence__translations__language__id=language)[:10]
-  variables = RequestContext(request, {
-     'form': form,
-     'words': words,
-     'languageid': language,
-     'show_results': show_results,
-  })
-  if request.GET.has_key('ajax'):
-    return render_to_response('word_list.html', variables)
-  else:
-    return render_to_response('glossary.html', variables)
+    form = SearchForm()
+    translations = []
+    language = 1
+    query = ''
+    show_results = False
+
+    if 'query' in request.GET:
+        show_results = True
+        query = request.GET['query'].strip()
+        language = request.GET['languages'].strip()
+        if query and language.isdigit():
+            form = SearchForm({'query' : query})
+
+            translations = Translation.objects.filter(
+                language__id=language).filter(sentence__words__term__icontains=query).order_by('sentence__length')
+
+    variables = RequestContext(request, {
+        'form': form,
+        'query': query,
+        'translations': translations,
+        'languageid': language,
+        'show_results': show_results,
+    })
+
+    if request.GET.has_key('ajax'):
+        return render_to_response('word_list.html', variables)
+    else:
+        return render_to_response('glossary.html', variables)
 
