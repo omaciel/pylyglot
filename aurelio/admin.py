7fdf9cb58b720f4e6a334d3688555d51adf522c6
Og B. Maciel <ogmaciel@gnome.org>
2010-08-09 18:49:30 -0400

    Added Language and Translation to admin forms.

--- aurelio/admin.py
+++ aurelio/admin.py
@@ -2,7 +2,13 @@
 # -*- encoding: utf-8 -*-
 
 from django.contrib import admin
-from aurelio.models import Sentence, Package, Word
+from aurelio.models import Sentence, Package, Word, Translation, Language
+
+class TranslationAdmin(admin.ModelAdmin):
+    pass
+
+class LanguageAdmin(admin.ModelAdmin):
+    pass
 
 class SentenceAdmin(admin.ModelAdmin):
     pass
@@ -13,6 +19,8 @@ class PackageAdmin(admin.ModelAdmin):
 class WordAdmin(admin.ModelAdmin):
     pass
 
+admin.site.register(Translation, TranslationAdmin)
+admin.site.register(Language, LanguageAdmin)
 admin.site.register(Sentence, SentenceAdmin)
 admin.site.register(Package, PackageAdmin)
 admin.site.register(Word, WordAdmin)
