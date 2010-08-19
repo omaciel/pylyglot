8f1cacb7ce14b6f916a7f50cb6ac7f7237f4588a
Og B. Maciel <ogmaciel@gnome.org>
2010-08-19 02:21:01 +0000

    New context_processors to provide generic estatistical information.

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,8 +1,18 @@
 # -*- encoding: utf-8 -*-
 # vim: ts=4 sw=4 expandtab ai
 
-from django.conf import settings
+from bidu.languages.models import Language
+from bidu.packages.models import Package
+from bidu.translations.models import Translation
 
-def templates_url(request):
+def total_packages(request):
 
-    return {'template_url': settings.TEMPLATE_DIRS}
+    return {'total_packages': Package.objects.count()}
+
+def total_languages(request):
+
+    return {'total_languages': Language.objects.count()}
+
+def total_translations(request):
+
+    return {'total_translations': Translation.objects.count()}
