f9a2c40041f4311123ca7758b4e0a05ad58bb6b7
Og B. Maciel <ogmaciel@gnome.org>
2010-08-20 14:59:21 +0000

    Back to querying Words table and then filtering it in the template.

--- translations/views.py
+++ translations/views.py
@@ -3,28 +3,31 @@
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from core.forms import SearchForm
+from bidu.core.forms import SearchForm
+from bidu.core.models import Word
 from bidu.translations.models import Translation
 
 def index(request):
 
     translations = []
+    language_id = 1
 
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
             query = form.cleaned_data['query']
-            language = form.cleaned_data['languages']
+            language_id = form.cleaned_data['languages']
 
-            translations = Translation.objects.filter(
-                language__id=language
-                ).filter(sentence__words__term__contains=query
-                ).order_by('sentence__length')
+            translations = Word.objects.filter(
+                term__contains=query
+                ).filter(sentence__translations__language__id=language_id
+                        ).distinct()
     else:
         form = SearchForm()
 
     variables = RequestContext(request, {
         'object_list': translations,
+        'language_id': language_id,
         'form': form,
         })
 
